151a903af290d5145b3099a877458b81e6699fc6
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2023-12-10 15:19:30 -0800

    the simple code

---
+++ pycode_style.py
@@ -0,0 +1,8 @@
+def greet(line):
+    """function to greet the user."""
+    greetings = f"hi, {line}! how are you doing?"
+    print(greetings)
+
+    if __name__ = "__main__":
+        the_user = input("enter your name: ")
+        greet(the_user)
